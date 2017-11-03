1f5b631cf050ec1bde2784981ff129538dd59a5a
rtv22 <rtvcojigor@gmail.com>
2017-11-03 20:11:05 +0300

    Update stack.cpp

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -1,9 +1,32 @@
+#include <stack.hpp>
 #include <catch.hpp>
 
-#include "stack.hpp"
+SCENARIO("init", "[init]") {
+	stack<int> A;
+	REQUIRE(A.count() == 0);
+}
 
-TEST_CASE("empty method for empty stack", "")
-{
-    stack<int> s;
-    REQUIRE( s.empty() );
+SCENARIO("Push", "[push]") {
+	stack<int> A;
+	A.push(1);
+	A.push(2);
+	REQUIRE(A.count() == 2);
+}
+
+SCENARIO("Pop", "[pop]") {
+	stack<int> A;
+	A.push(1);
+	A.push(2);
+	A.pop();
+	REQUIRE(A.count() == 1);
+}
+
+SCENARIO("oper=", "[oper]"){
+	stack<int> A;
+	stack<int> B;
+	A.push(1);
+	A.push(2);
+	REQUIRE(A.count() == 2);
+	B = A;
+	REQUIRE(B.count() == 2);
 }
